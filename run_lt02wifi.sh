9ddefc06d850a65545c3368abaf9126cf922dffc
alex-13021991 <alex13021991@gmail.com>
2015-12-07 14:27:52 +0600

    fix run_lt02wifi.sh

--- run_lt02wifi.sh
+++ run_lt02wifi.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-cd /home/dell/lt02/android_kernel_samsung_lt02
-. /home/dell/lt02/build.env
-mkdir -p /home/dell/lt02/build/lt02wifi
-make O=/home/dell/lt02/build/lt02wifi mrproper
-make O=/home/dell/lt02/build/lt02wifi pxa986_lt02wifi_blackhawk_defconfig
-make -j5 O=/home/dell/lt02/build/lt02wifi
-mkdir -p /home/dell/lt02/build/lt02wifi-bin
-mkdir -p /home/dell/lt02/build/lt02wifi-bin/modules
-cp /home/dell/lt02/build/lt02wifi/arch/arm/boot/zImage /home/dell/lt02/build/lt02wifi-bin/
-find /home/dell/lt02/build/lt02wifi/ -type f -name *.ko -exec cp {} /home/dell/lt02/build/lt02wifi-bin/modules/ \;
-cd /home/dell/lt02/android_kernel_samsung_lt02_modules
-make clean_modules KERNELDIR=/home/dell/lt02/build/lt02wifi KERNEL_OUTDIR=/home/dell/lt02/build/lt02wifi-bin
-make modules KERNELDIR=/home/dell/lt02/build/lt02wifi KERNEL_OUTDIR=/home/dell/lt02/build/lt02wifi-bin
+cd android_kernel_samsung_lt02
+. ~/lt02/build.env
+mkdir -p ~/lt02/build/lt02wifi
+make O=~/lt02/build/lt02wifi mrproper
+make O=~/lt02/build/lt02wifi pxa986_lt02wifi_blackhawk_defconfig
+make -j5 O=~/lt02/build/lt02wifi
+mkdir -p ~/lt02/build/lt02wifi-bin
+mkdir -p ~/lt02/build/lt02wifi-bin/modules
+cp ~/lt02/build/lt02wifi/arch/arm/boot/zImage ~/lt02/build/lt02wifi-bin/
+find ~/lt02/build/lt02wifi/ -type f -name *.ko -exec cp {} ~/lt02/build/lt02wifi-bin/modules/ \;
+cd ~/lt02/android_kernel_samsung_lt02_modules
+make clean_modules KERNELDIR=~/lt02/build/lt02wifi KERNEL_OUTDIR=~/lt02/build/lt02wifi-bin
+make modules KERNELDIR=~/lt02/build/lt02wifi KERNEL_OUTDIR=~/lt02/build/lt02wifi-bin
+cd ~/lt02/
