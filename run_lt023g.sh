3f72f56de5364db1b55882db769b73cb0beeab07
alex-13021991 <alex13021991@gmail.com>
2015-12-07 14:22:41 +0600

    fix run_lt023g.sh

--- run_lt023g.sh
+++ run_lt023g.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-cd /home/dell/lt02/android_kernel_samsung_lt02
-. /home/dell/lt02/build.env
-mkdir -p /home/dell/lt02/build/lt023g
-make O=/home/dell/lt02/build/lt023g mrproper
-make O=/home/dell/lt02/build/lt023g pxa986_lt023g_blackhawk_defconfig
-make -j5 O=/home/dell/lt02/build/lt023g
-mkdir -p /home/dell/lt02/build/lt023g-bin
-mkdir -p /home/dell/lt02/build/lt023g-bin/modules
-cp /home/dell/lt02/build/lt023g/arch/arm/boot/zImage /home/dell/lt02/build/lt023g-bin/
-find /home/dell/lt02/build/lt023g/ -type f -name *.ko -exec cp {} /home/dell/lt02/build/lt023g-bin/modules/ \;
-cd /home/dell/lt02/android_kernel_samsung_lt02_modules
-make clean_modules KERNELDIR=/home/dell/lt02/build/lt023g KERNEL_OUTDIR=/home/dell/lt02/build/lt023g-bin
-make modules KERNELDIR=/home/dell/lt02/build/lt023g KERNEL_OUTDIR=/home/dell/lt02/build/lt023g-bin
+cd android_kernel_samsung_lt02
+. ~/lt02/build.env
+mkdir -p ~/lt02/build/lt023g
+make O=~/lt02/build/lt023g mrproper
+make O=~/lt02/build/lt023g pxa986_lt023g_blackhawk_defconfig
+make -j5 O=~/lt02/build/lt023g
+mkdir -p ~/lt02/build/lt023g-bin
+mkdir -p ~/lt02/build/lt023g-bin/modules
+cp ~/lt02/build/lt023g/arch/arm/boot/zImage ~/lt02/build/lt023g-bin/
+find ~/lt02/build/lt023g/ -type f -name *.ko -exec cp {} ~/lt02/build/lt023g-bin/modules/ \;
+cd ~/lt02/android_kernel_samsung_lt02_modules
+make clean_modules KERNELDIR=~/lt02/build/lt023g KERNEL_OUTDIR=~/lt02/build/lt023g-bin
+make modules KERNELDIR=~/lt02/build/lt023g KERNEL_OUTDIR=~/lt02/build/lt023g-bin
+cd ~/lt02/
